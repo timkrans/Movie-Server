82bf072f450c8df7a3177c11d23c91775d6460f2
timkrans <tkransberger@yahoo.com>
2026-01-23 22:59:58 -0500

    Made a simple version of it going to add more features in the future

--- Backend/cmd/main.go
+++ Backend/cmd/main.go
@@ -1,9 +1,48 @@
-package main 
+package main
+
 import (
+	"net/http"
+	"os"
+	"path/filepath"
 
-
+	"github.com/gin-gonic/gin"
 )
 
-func main(){
-	
+const movieDir = "./movies"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/movies/:name", streamMovie)
+
+	r.Run(":8080")
+}
+
+func streamMovie(c *gin.Context) {
+	name := filepath.Base(c.Param("name"))
+	path := filepath.Join(movieDir, name)
+
+	file, err := os.Open(path)
+	if err != nil {
+		c.Status(http.StatusNotFound)
+		return
+	}
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	c.Header("Content-Type", "video/mp4")
+	c.Header("Accept-Ranges", "bytes")
+
+	http.ServeContent(
+		c.Writer,
+		c.Request,
+		stat.Name(),
+		stat.ModTime(),
+		file,
+	)
 }
