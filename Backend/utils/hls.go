0bbd0e30adc3defcc25301c59a0ef4d3a585953e
timkrans <tkransberger@yahoo.com>
2026-02-19 09:34:37 -0500

    Adding hls so seamless streaming

---
+++ Backend/utils/hls.go
@@ -0,0 +1,46 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func GenerateHLS(inputPath, outputDir string) (string, error) {
+	err := os.MkdirAll(outputDir, os.ModePerm)
+	if err != nil {
+		return "", err
+	}
+
+	playlist := fmt.Sprintf("%s/index.m3u8", outputDir)
+	//must be homebrw could be set as env variable
+	ffmpegPath := "/opt/homebrew/bin/ffmpeg"
+	/* recommend change 
+	ffmpegPath := os.Getenv("FFMPEG_PATH")
+	if ffmpegPath == "" {
+    	ffmpegPath = "ffmpeg" //fallback
+	}*/
+
+	cmd := exec.Command(ffmpegPath,
+		"-i", inputPath,
+		"-vf", "scale=-2:720",
+		"-c:v", "h264",
+		"-c:a", "aac",
+		"-ac", "2",
+		"-ar", "48000",
+		"-b:v", "2500k",
+		"-hls_time", "4",
+		"-hls_list_size", "0",
+		"-hls_segment_type", "mpegts",
+		"-hls_flags", "independent_segments",
+		playlist,
+	)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("FFMPEG ERROR:", string(output))
+		return "", err
+	}
+
+	return playlist, nil
+}
